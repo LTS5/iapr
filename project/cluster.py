bb48f02212d8bbada806cb7ec8eef1c9499c0faa
EliasWilliamGit <elias.william@hotmail.com>
2023-05-17 17:02:48 +0200

    Dummies for functions

---
+++ project/cluster.py
@@ -0,0 +1,2 @@
+# Input: list of feature vectors for each puzzle piece, list of the images in the same order
+# Output: list of list where the inner list is the images in the same puzzles, list of outliers
