e9b8cba9bc36293123a5f31bd1954147dd750b88
Sami Laubo <slaubo99@gmail.com>
2023-05-20 16:32:54 +0200

    comments and docs

--- project/segment.py
+++ project/segment.py
@@ -1,100 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-
 import os 
 from PIL import Image
+
 from scipy.signal import peak_prominences
-
-def segment_by_channels(image, plot_segmentation=False):
-    thresh_list = []
-
-    # Threshold every image channel
-    for i in range(3):
-        img = image[:, :, i]
-        thresh_new = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-        thresh_list.append(thresh_new)
-
-    # Threshold gray image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    thresh_gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    thresh_list.append(thresh_gray)
-
-    # Plot segmentations
-    if plot_segmentation:
-        fig, axs = plt.subplots(1, len(thresh_list))
-
-        for i in range(len(thresh_list)):
-            axs[i].imshow(thresh_list[i])
-            axs[i].set_xticks([]); axs[i].set_yticks([])
-
-        plt.tight_layout()
-        plt.show()
-
-    # Find contours
-    all_contours = []
-    for i in range(len(thresh_list)):
-        contours = cv2.findContours(thresh_list[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        # contours = cv2.findContours(thresh_list[i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours = contours[0] if len(contours) == 2 else contours[1]
-
-        all_contours += contours
-
-    return all_contours
-
-def segment_by_morph(image):
-    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # blur = cv2.GaussianBlur(gray, (13,13), 2)
-    # thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-        # plt.show()
-
-    # fig, axs = plt.subplots(1, 3)
-    # axs[0].plot(np.bitwise_and(thresh,thresh_list[2]))
-    # plt.show()
-
-    # return
-
-    # plt.imshow(gray)
-    # plt.show()
-    # plt.imshow(blur)
-    # plt.show()
-    # plt.imshow(thresh)
-    # plt.show()
-
-    # # Two pass dilate with horizontal and vertical kernel
-    # struc_size_1 = (10,5)
-    # struc_size_2 = (5,10)
-
-    # horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, struc_size_1)
-    # dilate = cv2.dilate(thresh, horizontal_kernel, iterations=1)
-    # vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, struc_size_2)
-    # dilate = cv2.dilate(dilate, vertical_kernel, iterations=1)
-
-    # # cv2.opening
-    # new_im = thresh.copy()
-    # close_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 50))
-    # open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
-    # new_im = cv2.morphologyEx(new_im, cv2.MORPH_CLOSE, close_kernel)
-    # new_im = cv2.morphologyEx(new_im, cv2.MORPH_OPEN, open_kernel)
-    # full_squares = new_im.copy()
-
-    # new_im = thresh.copy()
-    # close_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-    # open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-    # new_im = cv2.morphologyEx(new_im, cv2.MORPH_CLOSE, close_kernel)
-    # new_im = cv2.morphologyEx(new_im, cv2.MORPH_OPEN, open_kernel)
-    # sparse_squares = new_im.copy()
-
-
-    # plt.imshow(full_squares)
-    # plt.show()
-    # plt.imshow(sparse_squares)
-    # plt.show()
-
-    # plt.figure()
-    return
+from scipy.ndimage import rotate
 
 def segment_by_hist(image, plot_segmentation=False):
+    """
+    Segment image based on histogram max peak (assumed to be background)
+    
+    Parameters:
+        image: original image with background and puzzle pieces
+        plot_segmentation: plot intermediate results
+
+    Returns: 
+        segmented_images: [segmented image 1, segmented image 2, ...]
+    """
+    # Plot
     if plot_segmentation:
         print('Plots from: segment_by_hist')
         _, axs = plt.subplots(1, 4)
@@ -142,9 +66,20 @@ def segment_by_hist(image, plot_segmentation=False):
             axs[i].set_xticks([]); axs[i].set_yticks([])
         plt.show()
 
+    # Return binary images
     return segmented_images
 
 def segment_by_laplacian(image, plot_segmentation=False):
+    """
+    Segment image based on laplacian values
+    
+    Parameters:
+        image: original image with background and puzzle pieces
+        plot_segmentation: plot intermediate results
+
+    Returns: 
+        segmented_images: [segmented image 1, segmented image 2, ...]
+    """
     # Add gray channel
     channels = np.dstack((image, cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)))
 
@@ -168,51 +103,53 @@ def segment_by_laplacian(image, plot_segmentation=False):
             axs[i].set_xticks([]); axs[i].set_yticks([])
         plt.show()
 
-    return segmented_images
-
-
-def black_white_sharpening(segmented_images, plot_segmentation=False):
-    # Morphological operations
-    for i in range(len(segmented_images)):
-        close_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-        open_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
-
-        segmented_images[i] = cv2.morphologyEx(segmented_images[i], cv2.MORPH_OPEN, open_kernel)
-        segmented_images[i] = cv2.morphologyEx(segmented_images[i], cv2.MORPH_CLOSE, close_kernel)
-
-    # Plot binary images
-    if plot_segmentation:
-        print('Plots from: black_white_sharpening')
-
-        fig, axs = plt.subplots(1, 4)
-        for i in range(len(segmented_images)):
-            axs[i].imshow(segmented_images[i])
-            axs[i].set_xticks([]); axs[i].set_yticks([])
-        plt.show()
-
+    # Return binary images
     return segmented_images
 
 def find_contours(images):
+    """
+    Find all contours in segmented images
+    
+    Parameters:
+        images: list where each element is a binary image
+
+    Returns: 
+        all_contours: [contour1, contour2, ...]
+    """
     # Error handling
     if images is None:
         print('find_contours: Input image is None!')
         return []
 
-    # Find contours all contours in all images
+    # Find all contours in all images
     all_contours = []
     for i in range(len(images)):
-        contours = cv2.findContours(images[i], cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        # Extract contours
+        # RETR_EXTERNAL: return all contours without smaller encompasing contours 
+        # CHAIN_APPROX_SIMPLE: store only edge points in vertical or horisontal lines
+        contours = cv2.findContours(images[i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contours = contours[0] if len(contours) == 2 else contours[1]
 
+        # Appends since contours is a list
         all_contours += contours
 
+    # Return all extracted contours
     return all_contours
 
 def filter_contours(contours):
+    """
+    Remove bad contours and create bounding boxes around contours
     
+    Parameters:
+        contours: All contours
+
+    Returns: 
+        good_boxes: [box1, box2, ...] - One bounding box for each good contour
+    """
+    # Go through all contours
     good_boxes = []
     for c in contours:
-        # Get box coordinates
+        # Get minimal rectangle that encompases contour
         rot_rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rot_rect)
         box = np.int0(box)
@@ -235,9 +172,11 @@ def filter_contours(contours):
                 duplicate = True
                 break
 
+        # Add to good_boxes if passed tests and not in array from before
         if not duplicate:
             good_boxes.append(box)
 
+    # Return bounding boxes for all good boxes
     return good_boxes
 
 def refine_boxes(image, boxes, plot_intermediate=False):
@@ -297,7 +236,6 @@ def refine_boxes(image, boxes, plot_intermediate=False):
     # Return refined boxes
     return boxes
 
-from scipy.ndimage import rotate
 def extract_piece(image, box, plot_intermediate=False):
     """
     Extract pixels inside bounding box
@@ -366,18 +304,23 @@ def extract_pieces(image, boxes, plot_intermediate=False):
     return [extract_piece(image, box, plot_intermediate) for box in boxes]
 
 def find_puzzle_pieces(image, plot_results=False, plot_intermediate=False):
-    # Segment images by various methods
-    # segmented_images_hist = segment_by_hist(image, plot_segmentation=plot_intermediate)
-    # segmented_images_bw = black_white_sharpening(segmented_images_hist, plot_segmentation=plot_intermediate)
+    """
+    Extract individual puzzle pieces from image
+    
+    Parameters:
+        image: original image with background and puzzle pieces
+        plot_results: plot bounding boxes and extracted pieces
+        plot_intermediate: plot intermediate results from sub-functions
+
+    Returns: 
+        puzzle_pieces: [[128, 128, 3], ...] - Extracted puzzle pieces
+    """
+    
+    # Segment image by laplacian method
     segmented_images_laplacian = segment_by_laplacian(image, plot_segmentation=plot_intermediate)
 
-
-    # Get contours from all methods: + appends results since its normal lists
-    # all_contours = find_contours(segmented_images_hist)
-    # all_contours += find_contours(segmented_images_bw)
+    # Get contours from segmented image
     all_contours = find_contours(segmented_images_laplacian)
-    # all_contours += segment_by_channels(image) # += appends results since its normal lists
-    # all_contours += find_contours(image) # += appends results since its normal lists
 
     # Filter out by shape, size and duplicates
     boxes = filter_contours(all_contours)
@@ -385,42 +328,45 @@ def find_puzzle_pieces(image, plot_results=False, plot_intermediate=False):
     # Make sure they have correct size: if not try other method
     boxes = refine_boxes(image, boxes, plot_intermediate=plot_intermediate)
 
-    # Exract pieces
+    # Extract pieces from boxes
     puzzle_pieces = extract_pieces(image, boxes, plot_intermediate=plot_intermediate)
 
     # Plot results
     if plot_results:
+        # Plot image with boxes
         fig, axs = plt.subplots(1, 2)
         plt.title(f'Number of boxes: {len(boxes)}')
+        axs[0].imshow(image); axs[0].set_xticks([]); axs[0].set_yticks([])
+        axs[1].imshow(image); axs[1].set_xticks([]); axs[1].set_yticks([])
 
-        axs[0].imshow(image)
-        axs[1].imshow(image)
-
+        # Plot all bounding boxes
         for box in boxes:
             axs[1].plot(np.append(box[:, 0], box[0, 0]), np.append(box[:, 1], box[0, 1]), color='r')
         
-        axs[0].set_xticks([]); axs[0].set_yticks([])
-        axs[1].set_xticks([]); axs[1].set_yticks([])
+        # Make it look neat
         axs[1].set_xlim([0,image.shape[1]]); axs[1].set_ylim([image.shape[0],0])
         plt.tight_layout()
         plt.show()
 
+        # Plot extracted puzzle pieces
         fig, axs = plt.subplots((len(puzzle_pieces)+9) // 10, 10, figsize=(30,10))
         plt.suptitle(f'Extracted peices: {len(puzzle_pieces)}', fontsize=60)
         axs = axs.ravel()
 
+        # Remove ticks
         for ax in axs: 
             ax.set_xticks([]); ax.set_yticks([])
             ax.set_axis_off()
 
+        # Plot each piece in own subplot
         for ax, piece in zip(axs, puzzle_pieces): 
             ax.set_axis_on()
-            # print(f'{piece.shape = }')
             ax.imshow(piece)
 
         plt.tight_layout()
         plt.show()
 
+    # Return extracted puzzle pieces
     return puzzle_pieces
 
 if __name__ == '__main__':
@@ -428,26 +374,17 @@ if __name__ == '__main__':
         filename = "train_{}.png".format(str(image_index).zfill(2))
         return np.array(Image.open(os.path.join(path,folder,filename)).convert('RGB'))
 
-    # # boxes= []
+    num_pieces = []
+    import time
+    t1 = time.time()
     for i in range(12):
         test_image = load_input_image(i)
-        find_puzzle_pieces(test_image, plot_results=False, plot_intermediate=False)
+        num_pieces.append(len(find_puzzle_pieces(test_image, plot_results=True, plot_intermediate=False)))
+    t2 = time.time()
+    print(f'Time: {t2 - t1}')
 
-    # answers = np.array([28, 21, 28, 21, 20, 28, 29, 28, 27, 29, 28, 19])
-    # print(np.array(boxes) - answers)
+    answers = np.array([28, 21, 28, 21, 20, 28, 29, 28, 27, 29, 28, 19])
+    print(np.array(num_pieces) - answers)
 
     # test_image = load_input_image(2)
     # puzzle_boxes = find_puzzle_pieces(test_image, plot_results=True, plot_intermediate=False)
-
-
-    # Test on old data
-    # for i in range(5):
-    #     test_image = load_input_image(i, path="data_project_old")
-    #     segment(test_image, plot_results=True, plot_intermediate=False)
-
-    # Notes:
-    # laplacian works perfectly on new data
-    # laplacian does not work too good on old data
-    # Can add hist segmentation for robustness
-    # black_white sharpening adds boxes where there should not be boxes - don't use
-    # Segment by channels might be unnecessary, only slightly better than hist segmentation
